ba59efeed0fd8039902a6869e77197faf006e315
Abrar Jahin <66951426+ajpalok@users.noreply.github.com>
2020-08-15 18:41:26 +0600

    Create Worker.js

---
+++ assets/js/Worker.js
@@ -0,0 +1,31 @@
+{% assign image = '' %}
+    {% if page.image %}
+        {% assign image = page.image %}
+    {% else %}
+        {% assign image = "post.image" %}
+    {% endif %}
+const static2HAAS = "2HAAS-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "assets/css/main.css",
+  "assets/js/app.js",
+  "assets/img/off.jpg",
+  "{{ post.image }}",
+  "{{ page.image }}",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(static2HAAS).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
