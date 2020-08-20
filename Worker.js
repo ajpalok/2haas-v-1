d11128be97f78a757a79de18a79c24d63412ef95
Abrar Jahin <66951426+ajpalok@users.noreply.github.com>
2020-08-20 07:56:43 +0600

    Update Worker.js

--- Worker.js
+++ Worker.js
@@ -16,7 +16,6 @@ const assets = [
   '/assets/img/contact.jpg',
   '/assets/img/blog-image.png',
   '/assets/img/message.gif',
-  '/assets/js/app.js',
   '/assets/js/search-script.js',
   'https://identity.netlify.com/v1/netlify-identity-widget.js',
 ];
